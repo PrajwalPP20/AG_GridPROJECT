c8d4dab6eb388ed0d04d7720589c385c0b923766
Prajwal Patel <testvagrant@TestVagrants-MacBook-Pro-2.local>
2025-08-13 15:51:46 +0530

    store album details in a json file

--- tests/inventoryGrid.test.ts
+++ tests/inventoryGrid.test.ts
@@ -1,15 +1,18 @@
 import { test } from '@playwright/test';
 import { AppDemosPage } from '../pages/appDemosPage';
 import { InventoryGridPage } from '../pages/inventoryGridPage';
+import { mkdirSync, writeFileSync } from 'fs';
+import { join } from 'path';
 
 test.describe('AG Grid Inventory Page', () => {
+
 
     test('Verify extraction of album, artist, inventory and others info', async ({ page }) => {
         const appDemosPage = new AppDemosPage(page);
         await appDemosPage.goToInventoryPage();
         const inventoryGridPage = new InventoryGridPage(page);
-        const productsDetailsString: string = await inventoryGridPage.getAlbumDetails();
-        console.log(productsDetailsString);
+        const albumDetails: string = await inventoryGridPage.getAlbumDetails();
+        await inventoryGridPage.storeAlbumDetails(albumDetails);
     });
 
 });
