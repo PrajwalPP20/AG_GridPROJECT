1ac206df4963997867cdf3f5c1aa9d2d3457a598
Prajwal Patel <testvagrant@TestVagrants-MacBook-Pro-2.local>
2025-08-13 14:58:14 +0530

    extract album details from UI

--- tests/inventoryGrid.test.ts
+++ tests/inventoryGrid.test.ts
@@ -1,13 +1,17 @@
-import {test} from '@playwright/test';
+import { test } from '@playwright/test';
 import { AppDemosPage } from '../pages/appDemosPage';
+import { InventoryGridPage } from '../pages/inventoryGridPage';
 
 test.describe('AG Grid Inventory Page', () => {
 
-  test('Verify different coloumn in Inventory Page', async ({page}) => {
-      const appDemosPage = new AppDemosPage(page);
-      await appDemosPage.goToInventoryPage();
-
-});
+    test('Verify extraction of album, artist, inventory and others info', async ({ page }) => {
+        const appDemosPage = new AppDemosPage(page);
+        await appDemosPage.goToInventoryPage();
+        await page.waitForTimeout(5000)
+        const inventoryGridPage = new InventoryGridPage(page);
+        const productsDetailsString: string = await inventoryGridPage.getAlbumDetails();
+        console.log(productsDetailsString);
+    });
 
 });
 
---
+++ types/Product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    albumName: string;
+    artist: string;
+    year: number;
+    status: string;
+    price: string;
+    inventory: string;
+    incoming: number;
+    sold: number;
+    estimatedProfit: string;
+    actions: string;
+}
