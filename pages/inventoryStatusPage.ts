e1af6a448837d22fd2e70ad6dc21fdd3e35956ea
Prajwal Patel <testvagrant@TestVagrants-MacBook-Pro-2.local>
2025-08-13 17:32:51 +0530

    refactor code

---
+++ pages/inventoryStatusPage.ts
@@ -0,0 +1,14 @@
+import { Locator, Page } from "@playwright/test";
+
+export class InventoryStatusPage {
+    readonly page: Page;
+    readonly inventoryAciveStatusButton: Locator;
+    readonly inventoryOnHoldStatusButton: Locator;
+
+
+    constructor(page: Page) {
+        this.page = page;
+        this.inventoryAciveStatusButton = page.getByRole('button', { name: 'Active' });
+        this.inventoryOnHoldStatusButton = page.getByRole('button', { name: 'On Hold' });
+    }
+}
