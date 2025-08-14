f0802f795eb59bf4f606b2ccfc21257554a8aa03
Prajwal Patel <testvagrant@TestVagrants-MacBook-Pro-2.local>
2025-08-14 14:51:51 +0530

    files attached

--- global-setup.ts
+++ global-setup.ts
@@ -3,10 +3,12 @@ import fs from 'fs';
 import path from 'path';
 import { chromium } from 'playwright';
 import { BasePage } from './pages/BasePage';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '.env.development') });
 
 // This global setup will run once before all tests
 async function globalSetup() {
-    const baseUrl: string = 'https://www.ag-grid.com/example-inventory/';
+    const baseUrl: string = process.env.BASE_URL + (process.env.INVENTORY_PATH || '/example-inventory/');
     const albumDetailsPath = path.resolve(__dirname, 'test-data', 'album-details.json');
     if (!fs.existsSync(albumDetailsPath)) {
         const browser = await chromium.launch();
