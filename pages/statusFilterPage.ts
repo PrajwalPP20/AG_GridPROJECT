29fc92ff6369af479d66612c26e7a0e60d98a94e
TestVagrant <testvagrant@Pankaj.local>
2025-08-19 12:14:54 +0530

    few change

--- pages/statusFilterPage.ts
+++ pages/statusFilterPage.ts
@@ -13,19 +13,19 @@ export class StatusFilterPage {
         this.filterSearchBar = page.getByPlaceholder("Search...");
     }
 
-    async filterStatus(status: string) {
+    async filterAlbumStatus(status: string) {
         const inventoryGridPage = new InventoryGridPage(this.page);
         await inventoryGridPage.statusFilterDropdown.click();
         const statusFilterPage = new StatusFilterPage(this.page);
         await statusFilterPage.filterSearchBar.fill(status);
-        await statusFilterPage.filterSearchBar.press('Enter');
+        await statusFilterPage.filterSearchBar.press('Enter', { delay: 1000 });
+        await this.page.keyboard.press('Escape'); // Close the filter dropdown
+        // Validate the status in the inventory grid
+        if (this.activeStatus === status || this.onHoldStatus === status || this.outOfStock === status) {
+            const inventoryGrdPage = new InventoryGridPage(this.page);
+            await inventoryGrdPage.validateAlbumStatus(status);
+        } else {
+            throw new Error(`Invalid status: ${status}. Valid statuses are: ${this.activeStatus}, ${this.onHoldStatus}, ${this.outOfStock}`);
+        }
     }
-
-
-
-
-
-
-
-
 }       
